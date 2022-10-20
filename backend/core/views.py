d2739bcc33294ed48115b75dce39ef1e8462af53
Sherwin <sherwinzhang100@gmail.com>
2022-10-19 22:57:41 -0700

    halfway through the flask+python tutorial

--- backend/core/views.py
+++ backend/core/views.py
@@ -25,4 +25,12 @@ def remove_background_post():
   url = data['data']['url']
 
   return render_template('show-result.html', text=url)
-  
+
+@app.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
