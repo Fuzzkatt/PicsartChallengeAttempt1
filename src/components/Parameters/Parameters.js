0d14e216c340089b6fa716f35f447c0e46842920
Sherwin <sherwinzhang100@gmail.com>
2022-10-26 15:11:25 -0700

    adding front end parameters

--- src/components/Parameters/Parameters.js
+++ src/components/Parameters/Parameters.js
@@ -4,17 +4,18 @@ import './Parameters.css';
 const Parameters = () => {
   return (
     <div className="container3">
-      <ul className="list">
-        <div className="param">Remove text</div>
-        <div className="param">Image Upscale</div>
-        <div className="param">Remove Background</div>
-        <div className="param">De-noising/Remove Splotches</div>
-        <div className="param">Image Contrast</div>
-        <div className="param">Black & White Image</div>
-      </ul>
+      <table>
+      <tr><td><label className="param" for="removeText">Remove text</label></td><td><input type="checkbox" className="removeText"/></td></tr>
+      <tr><td><label for="ImageUpscale" className="param">Image Upscale</label></td><td><input  type="checkbox" className="ImageUpscale"/></td></tr>
+      <tr><td><label for="removeBackground"className="param">Remove Background   </label></td><td><input type="checkbox" className="removeBackground"/></td></tr>
+      <tr><td><label for="removeSplotches" className="param">Remove Splotches</label></td><td><input type="checkbox" className="removeSplotches"/></td></tr>
+      <tr><td><label for="imageContrast" className="param">Image Contrast</label></td><td> <input type="range" min="1" max="100" className="imageContrast slider"/></td></tr>
+      <tr><td><label className="param">Black & White Image</label></td><td> <input type="checkbox" className="BlackAndWhite"/></td></tr>
+      </table>
       <button className="generate">Generate Image</button>
     </div>
   )
 }
 
 export default Parameters
+
